6061b47c093c41f04cfd24b05b4f9e556e9a1777
Dyasoma <ddieasoma@gmail.com>
2025-12-28 04:40:17 -0700

    small changes

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ from objects.game_state import GameState
 from objects.team import Team
 
 
-FPS = 60
+FPS = 60   # frames per second
 
 
 def setup():
     pygame.init()
     window = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    pygame.display.set_caption("Chess")  # window title
+    pygame.display.set_caption('Chess')  # window title
     window.fill(GREY)
     chessboard = GameBoard(
         SQUARESIZE, SQUARECOUNT, DARKCOLOR, LIGHTCOLOR, window
@@ -36,36 +36,24 @@ def setup():
     pygame.display.update()
     black_player = Team(BLACKPLAYER, BLACK)
     white_player = Team(WHITEPLAYER, WHITE)
-    chessboard.set_pieces(black_player.active_pieces, white_player.active_pieces)
+    chessboard.set_pieces(
+        black_player.active_pieces, white_player.active_pieces
+    )
     return (pygame.time.Clock(), chessboard, black_player, white_player)
 
 
 def main():
     # Setup and Initialization
-    delta_report = 0
     clock, chessboard, black_player, white_player = setup()
     game_state = GameState(chessboard, black_player, white_player)
     while game_state.get_game_is_running():
         game_state.handle_events()
         game_state.update_state()
         game_state.render()
-        delta_report += 1
         clock.tick(FPS)
-        """        
-        if delta_report == 50:
-            print("\n"*2)
-            print(f"Current Player : {game_state.print_current_player()}")
-            print(f"Black Pieces {black_player.get_count_active()}")
-            print(f"Captured Black Pieces : {black_player.get_count_captured()}")
-            print(f"White Pieces {white_player.get_count_active()}")
-            print(f"Captured White Pieces : {white_player.get_count_captured()}")
-            print("\n"*2)
-            delta_report = 0
-        """
-
         pygame.display.update()
     pygame.quit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
